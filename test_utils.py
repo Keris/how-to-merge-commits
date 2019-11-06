03978f2a878d00888029a9f8d8d0c8e6d4651c6a
keris <dlq137@gmail.com>
2019-11-06 15:23:44 +0800

    Add a utility module and its test driver

---
+++ test_utils.py
@@ -0,0 +1,7 @@
+from utils import greet_user, greet
+
+
+if __name__ == '__main__':
+    user = 'dulq'
+    msg = greet_user(user)
+    greet(msg)
---
+++ utils.py
@@ -0,0 +1,11 @@
+def greet_user(user):
+    return "Welcome you, {}".format(user)
+
+
+def greet(msg):
+    r"""Prints a greeting message.
+
+    Args:
+        msg (str): a greeting message.
+    """
+    print(msg)
